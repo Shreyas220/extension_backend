399e767db1988e38bd8a1c1b310e24ebbe34739d
Shreyas220 <shreyas.ny@gmail.com>
2021-04-30 06:15:53 +0530

    to check if data already present

--- extension_backend/core/api.py
+++ extension_backend/core/api.py
@@ -4,19 +4,56 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import TimespentSerializer
 from .models import timespent
+from rest_framework import status
+import json
+
+
+
 @api_view(['POST'])
 def postDetail(request):
-    serializer = TimespentSerializer(data=request.data)
-    if(serializer.is_valid):
-        serializer.save
+    request_data = json.loads(request.body)
 
-    return Response(serializer.data)
+    try:
+        #check if the site exists 
+        site = timespent.objects.get(url=request_data['url'])
+
+
+    except site.DoesNotExist:
+        postDetailNew(request.body)
+
+    else:
+        postDetailAdd(request.body)
+
+
+
+def postDetailAdd(data_site):
+    request_data = json.loads(request.body)
+    site = timespent.objects.get(url=request_data['url'])
+    request_data['timepassed'] =  request_data['timepassed'] + site['timepassed'] 
+
+    serializer = TimespentSerializer(site, data=request_data)
+
+
+
+def postDetailNew(data_site):
+    serializer = TimespentSerializer(data=data_site)
+    try:
+        serializer.is_valid(raise_exception=True)
+
+    except:
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    else:
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    
 
 @api_view(['GET'])
 def getAllDetail(request):
     timeData = timespent.objects.all()
     serializer = TimespentSerializer(timeData, many = True )
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
     
-    
+
 
